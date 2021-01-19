212d98e852eaf6801a53879faeecc9c5a07c952b
matheus dalmolin <mtsdalmolin@gmail.com>
2021-01-19 16:06:33 -0300

    add arg error handler in client.py

--- client.py
+++ client.py
@@ -3,6 +3,9 @@ import rpyc
 import sys
 
 if __name__ == "__main__":
+  if len(sys.argv) < 2:
+      print('Choose between \'load\' and \'train\' functions')
+      exit()
   func = sys.argv[1]
   c = rpyc.connect("localhost", 12345)
   exec('print(c.root.{}())'.format(func))
